1e24f68c4c7a2ba81f9cd8e48db8108c58460e1a
João Vítor Fernandes <thalesfernr@gmail.com>
2025-04-16 22:32:48 -0300

    Entidade User e Recurso UserResource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Someone", "someone@gmail.com", "(12) 3456-7890", "someone123");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
